c30e4044bbf37fb069f6e84009613c292eaebbd9
Guilherme Fiola <guilhermefiola@gmail.com>
2021-10-30 00:08:52 -0300

    Minimal APIs para .Net 5 e .Net 6

---
+++ MinimalApiNet5/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+var webHost = new WebHostBuilder()
+    .UseKestrel()
+    .ConfigureServices(services => {
+        services.AddRouting();
+        services.AddSingleton<ClienteRepository>();
+    })
+    .Configure(app => {
+        app.UseRouting();
+        
+        // .NET Core 3
+        var repo = app.ApplicationServices.GetService<ClienteRepository>();
+        
+        app.UseEndpoints(endpoints => {
+            
+            endpoints.MapGet("/clientes", context => {
+                var clientes = repo.GetAll();
+                return context.Response.WriteAsJsonAsync(clientes);
+            });
+
+            endpoints.MapGet("/clientes/{id}", context => {
+                context.Request.RouteValues.TryGetValue("id", out var id);
+                var idGuid = Guid.Parse(id.ToString());
+                var cliente = repo.GetById(idGuid);
+
+                if (cliente is null) {
+                    context.Response.StatusCode = 404;
+                    return context.Response.CompleteAsync();
+                }
+
+                return context.Response.WriteAsJsonAsync(cliente);
+            });
+
+            endpoints.MapPost("/clientes", context => {
+                var bodyStr = "";
+                var req = context.Request;
+
+                using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8, true, 1024, true))
+                {
+                    bodyStr = Task.Run(async () => await reader.ReadToEndAsync()).Result;
+                }
+                
+                var cliente = JsonSerializer.Deserialize<Cliente>(bodyStr);
+                repo.Create(cliente);
+
+                context.Response.StatusCode = 201;
+                return context.Response.WriteAsJsonAsync(cliente);
+            });
+
+            endpoints.MapPut("/clientes", context => {
+                var bodyStr = "";
+                var req = context.Request;
+
+                using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8, true, 1024, true))
+                {
+                    bodyStr = Task.Run(async () => await reader.ReadToEndAsync()).Result;
+                }
+                
+                var clienteUpdate = JsonSerializer.Deserialize<Cliente>(bodyStr);
+                
+                var cliente = repo.GetById(clienteUpdate.Id);
+                
+                if (cliente is null) {
+                    context.Response.StatusCode = 404;
+                    return context.Response.CompleteAsync();
+                }
+                
+                repo.Update(clienteUpdate);
+
+                return context.Response.WriteAsJsonAsync(clienteUpdate);
+            });
+
+            endpoints.MapDelete("/clientes/{id}", context => {
+                context.Request.RouteValues.TryGetValue("id", out var id);
+                var idGuid = Guid.Parse(id.ToString());
+
+                repo.Delete(idGuid);
+
+                return context.Response.CompleteAsync();
+            });
+        });
+    })
+    .Build();
+
+webHost.Run();
+
+record Cliente(Guid Id, string NomeCompleto);
+
+class ClienteRepository
+{
+    private readonly Dictionary<Guid, Cliente> _clientes = new();
+
+    public void Create(Cliente cliente) {
+        if (cliente is null) return;
+        _clientes[cliente.Id] = cliente;
+    }
+
+    public Cliente GetById(Guid id) {
+        var retorno = _clientes.TryGetValue(id, out Cliente cliente);
+        return cliente;
+    }
+
+    public List<Cliente> GetAll() {
+        return _clientes.Values.ToList();
+    }
+
+    public void Update(Cliente cliente) {
+        var clienteExistente = GetById(cliente.Id);
+        if (clienteExistente is null) return;
+        _clientes[cliente.Id] = cliente;
+    }
+
+    public void Delete(Guid id) {
+        _clientes.Remove(id);
+    }
+}
---
+++ MinimalApiNet6/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ClienteRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.MapGet("/clientes", ([FromServices] ClienteRepository repo) => 
+{
+    return repo.GetAll();
+});
+
+app.MapGet("/clientes/{id}", ([FromServices] ClienteRepository repo, Guid id) => 
+{
+    var cliente = repo.GetById(id);
+    return cliente is not null ? Results.Ok(cliente) : Results.NotFound();
+});
+
+app.MapPost("/clientes", ([FromServices] ClienteRepository repo, Cliente cliente) => 
+{
+    repo.Create(cliente);
+    return Results.Created($"/clientes/{cliente.Id}", cliente);
+});
+
+app.MapPut("/clientes", ([FromServices] ClienteRepository repo, Guid id, Cliente clienteUpdate) => 
+{
+    var cliente = repo.GetById(id);
+    if (cliente is null) return Results.NotFound();
+    repo.Update(clienteUpdate);
+    return Results.Ok(clienteUpdate);
+});
+
+app.MapDelete("/clientes/{id}", ([FromServices] ClienteRepository repo, Guid id) => 
+{
+    repo.Delete(id);
+    return Results.Ok();
+});
+
+app.UseSwaggerUI();
+
+app.Run();
+
+record  Cliente(Guid Id, string NomeCompleto);
+
+class ClienteRepository
+{
+    private readonly Dictionary<Guid, Cliente> _clientes = new();
+
+    public void Create(Cliente cliente) {
+        if (cliente is null) return;
+        _clientes[cliente.Id] = cliente;
+    }
+
+    public Cliente GetById(Guid id) {
+        return _clientes[id];
+    }
+
+    public List<Cliente> GetAll() {
+        return _clientes.Values.ToList();
+    }
+
+    public void Update(Cliente cliente) {
+        var clienteExistente = GetById(cliente.Id);
+        if (clienteExistente is null) return;
+        _clientes[cliente.Id] = cliente;
+    }
+
+    public void Delete(Guid id) {
+        _clientes.Remove(id);
+    }
+}
